e54502518a613999d29786a31c6ff62caf32f67f
omeryildizce <omeryildizce@gmail.com>
2023-05-01 10:18:23 +0300

    Update MainActivity.java

--- s02_LuckyNumber/LuckyNumber/app/src/main/java/com/omeryildizce/luckynumber/MainActivity.java
+++ s02_LuckyNumber/LuckyNumber/app/src/main/java/com/omeryildizce/luckynumber/MainActivity.java
@@ -2,13 +2,35 @@ package com.omeryildizce.luckynumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    Button button;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                Intent intent = new Intent(getApplicationContext(),  LuckyActivity.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
     }
 }
